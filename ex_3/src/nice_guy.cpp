21c537538e656f75025bf9fab4a30eee414832e5
Micha <mlk04@wp.pl>
2018-04-14 11:39:48 +0200

    subscribing_and_publishing

--- ex_3/src/nice_guy.cpp
+++ ex_3/src/nice_guy.cpp
@@ -2,25 +2,29 @@
 #include "std_msgs/String.h"
 #include <sensor_msgs/JointState.h>
 #include "geometry_msgs/PoseStamped.h"
-
 #include <sstream>
 
-//void callbackJointState(const JointStateConstPtr& state)
-//{
- // ROS_INFO("I heard: [%s]", msg->data.c_str());
-//}
+double angle[2];// angles from joint_state_publisher
+
+void callbackJointState(const sensor_msgs::JointState::ConstPtr& state)
+{
+  for(int i; i<3; i++)
+  {
+   	angle[i]=state->position[i];
+   	std::cout<<"I heard : "<<state->name[i]<<" , value: "<<angle[i]<<std::endl;
+  }
+}
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "NONKDL_DKIN");
   
-  geometry_msgs::PoseStamped msg; //obiekt PoseStamped
+  geometry_msgs::PoseStamped msg;
   ros::NodeHandle n;
 
   ros::Publisher poseStampedPub = n.advertise<geometry_msgs::PoseStamped>("poseStampedDsr", 1); 
 
-  //ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-  //ros::Subscriber joint_state = n_.subscribe("joint_states", 10, callbackJointState);
+  ros::Subscriber joint_state = n.subscribe("joint_states", 10, callbackJointState);
 
   ros::Rate loop_rate(1);
 
@@ -31,19 +35,20 @@ int main(int argc, char **argv)
 
     msg.header.frame_id="/base_link";
     msg.header.stamp = ros::Time::now();
+
+
     msg.pose.position.x =0;
     msg.pose.position.y = 0;
     msg.pose.position.z = 0;
 
-   msg.pose.orientation.x = -4.88070518543e-05;
-   msg.pose.orientation.y = -0.70080730609;
-   msg.pose.orientation.z = 7.50503570579e-05;
-   msg.pose.orientation.w = 0.713350613677;
+    msg.pose.orientation.x = -4.88070518543e-05;
+    msg.pose.orientation.y = -0.70080730609;
+    msg.pose.orientation.z = 7.50503570579e-05;
+    msg.pose.orientation.w = 0.713350613677;
 
-ROS_INFO("poseStamped orientation: %lf, %lf, %lf, %lf",
-msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z,msg.pose.orientation.w);
 
-    poseStampedPub.publish(msg);
+
+    poseStampedPub.publish(msg); //sending geometry_msgs
     ros::spinOnce();
 
     loop_rate.sleep();
