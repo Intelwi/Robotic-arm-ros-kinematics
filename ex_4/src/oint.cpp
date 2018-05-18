74fbed884e49d940f3e51e8891b0d199ae3c85c4
Micha <mlk04@wp.pl>
2018-05-18 10:20:56 +0200

    working oint

--- ex_4/src/oint.cpp
+++ ex_4/src/oint.cpp
@@ -7,15 +7,15 @@
 #include "ex_4/OintControlSrv.h"
 
 ros::Publisher poseStatePub1;
-float teta_0[3]; // y(0)
-float teta_solv[3]; // rozwiazania
-float position_0[3]; //współrzędne początkowe xyz 
-float position_solv[3]; //współrzędne wyliczone
-double qaternion[3];
+double teta_0[3]; // y(0)
+double teta_solv[3]; // rozwiazania
+double position_0[3]; //współrzędne początkowe xyz 
+double position_solv[3]; //współrzędne wyliczone
+double qaternion[4];
 int sampling = 60;
 int k=0;
 int status;
-float ttime, teta1, teta2, teta3,x,y,z;
+double ttime, teta1, teta2, teta3,x,y,z;
 
 
 // interpolacja liniowa, time-czas wykonania ruchu, tetaX - zadane kolejne kąty
@@ -26,14 +26,26 @@ int linear_inter(int mode)
 
 	
 	ros::Rate loop_rate(50);
-	float a1 = -(teta1 - teta_0[0]);
-	float b1 = teta1 - teta_0[0];
 
-	float a2 = -(teta2 - teta_0[1]);
-	float b2 = teta2 - teta_0[1];
+	//------Obliczenia dla metody trapezowej-------------->>>
+		//-----katy
+	double a1 = -(teta1 - teta_0[0]);
+	double b1 = teta1 - teta_0[0];
 
-	float a3 = -(teta3 - teta_0[2]);
-	float b3 = teta3 - teta_0[2];
+	double a2 = -(teta2 - teta_0[1]);
+	double b2 = teta2 - teta_0[1];
+
+	double a3 = -(teta3 - teta_0[2]);
+	double b3 = teta3 - teta_0[2];
+		//-----pozycje
+	double c1 = -(x - teta_0[0]);
+	double d1 = x - teta_0[0];
+
+	double c2 = -(y - teta_0[1]);
+	double d2 = y - teta_0[1];
+
+	double c3 = -(z - teta_0[2]);
+	double d3 = z - teta_0[2];
 
 	//-----------------------counting loop:--------------->>>
 
@@ -66,19 +78,26 @@ int linear_inter(int mode)
 			teta_solv[2] = teta_0[2] + (teta3 - teta_0[2])*k/(ttime*sampling);
 		}
 
-	//---------2nd mode next tetas:######### NIE OBSŁUŻONE #######################
+	//---------2nd mode next tetas:
 
 
-		/*else if(mode == 2)
+		else if(mode == 2)
 		{
 			float tx = k/(ttime*sampling);
-
+			//---------kąty----------------
 			teta_solv[0] = (1-tx)*teta_0[0] + tx*teta1 + tx*(1-tx)*(a1*(1-tx) + b1*tx);
 
 			teta_solv[1] = (1-tx)*teta_0[1] + tx*teta2 + tx*(1-tx)*(a2*(1-tx) + b2*tx);
 
 			teta_solv[2] = (1-tx)*teta_0[2] + tx*teta3 + tx*(1-tx)*(a3*(1-tx) + b3*tx);
-		}*/
+			//---------pozycja--------------
+			position_solv[0] = (1-tx)*position_0[0] + tx*x + tx*(1-tx)*(c1*(1-tx) + d1*tx);
+
+			position_solv[1] = (1-tx)*position_0[1] + tx*y + tx*(1-tx)*(c2*(1-tx) + d2*tx);
+
+			position_solv[2] = (1-tx)*position_0[2] + tx*z + tx*(1-tx)*(c3*(1-tx) + d3*tx);
+		
+		}
 
 		else {
 			ROS_WARN("\nNie ma takiego trybu.\n");
@@ -86,40 +105,12 @@ int linear_inter(int mode)
 		}
 
 //---------sending:
-		KDL::Rotation roll(KDL::Vector(1,0,0),
-                     	KDL::Vector(0,cos(teta_solv[0]),sin(teta_solv[0])),
-			KDL::Vector(0,-sin(teta_solv[0]),cos(teta_solv[0])));
 
-		KDL::Rotation pitch(KDL::Vector(cos(teta_solv[1]),0,-sin(teta_solv[1])),
-                     	KDL::Vector(0,1,0),
-			KDL::Vector(sin(teta_solv[1]),0,cos(teta_solv[1])));
+		KDL::Rotation r1=KDL::Rotation::RPY(teta_solv[0],teta_solv[1],teta_solv[2]);
 
-		KDL::Rotation yaw(KDL::Vector(cos(teta_solv[2]),sin(teta_solv[2]),0),
-                     	KDL::Vector(-sin(teta_solv[2]),cos(teta_solv[2]),0),
-			KDL::Vector(0,0,1));
-		KDL::Rotation r1 = roll*pitch*yaw;
-
-/*
-
-		KDL::Vector v1(cos(teta_solv[2])*cos(teta_solv[1]),sin(teta_solv[2])*cos(teta_solv[1]),-sin(teta_solv[1]));
-		KDL::Vector v2(cos(teta_solv[2])*sin(teta_solv[1])*sin(teta_solv[0])-sin(teta_solv[2])*cos(teta_solv[0]),sin(teta_solv[2])*sin(teta_solv[1])*sin(teta_solv[0])+cos(teta_solv[2])*cos(teta_solv[0]),cos(teta_solv[1])*sin(teta_solv[0]));
-		KDL::Vector v3(cos(teta_solv[2])*sin(teta_solv[1])*cos(teta_solv[0])+sin(teta_solv[2])*sin(teta_solv[0]),sin(teta_solv[2])*sin(teta_solv[1])*cos(teta_solv[0])-cos(teta_solv[2])*sin(teta_solv[0]),cos(teta_solv[1])*cos(teta_solv[0]));
-
-		 v1.Normalize();
-		 v2.Normalize();
-		 v3.Normalize();
-		KDL::Rotation r1(v1,v2,v3);
-
-
-		KDL::Rotation r1(KDL::Vector(cos(position_solv[2])*cos(position_solv[1]),sin(position_solv[2])*cos(position_solv[1]),-sin(position_solv[1])),
-                     KDL::Vector(cos(position_solv[2])*sin(position_solv[1])*sin(position_solv[0])-sin(position_solv[2])*cos(position_solv[0]),sin(position_solv[2])*sin(position_solv[1])*sin(position_solv[0])+cos(position_solv[2])*cos(position_solv[0]),cos(position_solv[1])*sin(position_solv[0])),
-                     KDL::Vector(cos(position_solv[2])*sin(position_solv[1])*cos(position_solv[0])+sin(position_solv[2])*sin(position_solv[0]),sin(position_solv[2])*sin(position_solv[1])*cos(position_solv[0])-cos(position_solv[2])*sin(position_solv[0]),cos(position_solv[1])*cos(position_solv[0])));
-*/
-
-		//std::cout<<vr[0]<<" "<<vr[1]<<" "<<vr[2]<<std::endl;
 		//--------Getting_Quaternions-------------
 
-		   //r1.GetQuaternion(qaternion[0],qaternion[1],qaternion[2],qaternion[3]);
+		   r1.GetQuaternion(qaternion[0],qaternion[1],qaternion[2],qaternion[3]);
 
 		//-----------------------------------------    
 		    msg.pose.position.x = position_solv[0];
@@ -127,10 +118,10 @@ int linear_inter(int mode)
 		    msg.pose.position.z = position_solv[2];
 
 
-		    //msg.pose.orientation.x = qaternion[0];
-		    //msg.pose.orientation.y = qaternion[1];
-		    //msg.pose.orientation.z = qaternion[2];
-		    //msg.pose.orientation.w = qaternion[3];
+		    msg.pose.orientation.x = qaternion[0];
+		    msg.pose.orientation.y = qaternion[1];
+		    msg.pose.orientation.z = qaternion[2];
+		    msg.pose.orientation.w = qaternion[3];
 
 		poseStatePub1.publish(msg);
 		ros::spinOnce();
