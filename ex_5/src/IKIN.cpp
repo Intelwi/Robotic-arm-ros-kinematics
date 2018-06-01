f91d914502af98838cccbea6c318303c46bc59f0
Micha <mlk04@wp.pl>
2018-06-01 19:24:36 +0200

    IKIN baza

--- ex_5/src/IKIN.cpp
+++ ex_5/src/IKIN.cpp
@@ -4,15 +4,36 @@
 #include "geometry_msgs/PoseStamped.h"
 #include <std_msgs/Float64.h>
 #include <sstream>
+#include <cmath>
 
-//współrzędne końcówki
-double x=0;
-double y=0;
-double z=0;
+//nowe współrzędne końcówki
+double x = 0.4;
+double y = 0;
+double z = 0.4;
+
+//stare współrzędne końcówki
+double x_old = 0.4;
+double y_old = 0;
+double z_old = 0.4;
+
+//długości członów
+double link_1;
+double link_2;
+
+//długości podstaw
+double base = 0.2;
+double rot_base = 0.1;
+
+//poszczególne kąty w stawach
+double rotate;
+double flex1;
+double flex2;
+double flex_end;
 
 //odebranie wiadomości ze współrzędnymi końcówki
 void callbackEndState(const geometry_msgs::PoseStamped::ConstPtr& state)
 {
+
 	x = state->pose.position.x;
 	y = state->pose.position.y;
 	z = state->pose.position.z;
@@ -31,9 +52,20 @@ int main(int argc, char **argv)
 
 	ros::Rate loop_rate(50);
 	
+	//pobranie z serwera parametrów
+	bool ok0 = n.getParamCached("a2_length", link_1);
+	bool ok1 = n.getParamCached("a3_length", link_2);
+
+	if ( !ok0 || !ok1 )
+	{
+			puts("ERROR OCCURED"); 
+			exit(1);
+	}
+	
 	//rozwiązywanie OZK i wysłanie do RobotStatePublisher
 	while(ros::ok())
 	{
+
 		sensor_msgs::JointState msg;
 		msg.header.stamp = ros::Time::now();
 		
@@ -43,17 +75,32 @@ int main(int argc, char **argv)
 			Rozwiazywanie zadania kinematyki odwrotnej
 		*/
 
+		//wyliczenie ąta dla bazy obrotowej <-----------------------------------NIE DZIAŁA XD
+		double a = sqrt( pow(x,2) + pow(y,2) );
+		double b = sqrt( pow(x_old,2) + pow(y_old,2) );
+		double c = sqrt( pow(abs(x-x_old),2) + pow(abs(y-y_old),2) );
 
+		rotate = acos( ( (pow(a,2) + pow(b,2) - pow(c,2) ) / ( 2*a*b ) ) );
+
+		if(rotate>1 && rotate<-1) std::cout<<"zle katy bazy"<<std::endl;
+
+		std::cout<<"arg acos: "<<(pow(a,2) + pow(b,2) - pow(c,2) ) / ( 2*a*b )<<" "<<std::endl;
+		//KONIEC###########################################
+
+
+		std::cout<<"kat baza: "<<rotate<<" "<<std::endl;
 		msg.name.push_back("rotation_joint"); //msg.position.push_back();
 		msg.name.push_back("shoulder");
 		msg.name.push_back("elbow");
 
-		msg.position.push_back(1);
+		msg.position.push_back(rotate);
 		msg.position.push_back(-0.5);
 		msg.position.push_back(0.3);
 
 		JointStatePub.publish(msg);
-
+		
+		
+		
 		ros::spinOnce();
 
 		loop_rate.sleep();
